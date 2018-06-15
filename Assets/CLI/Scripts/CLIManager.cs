05a42ae0b15e59a941068eedee2fb2bd92f3f8b6
FireFlame74 <sandro.figo@gmx.at>
2018-06-15 11:05:53 +0200

    Added CLIManager

---
+++ Assets/CLI/Scripts/CLIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CLIManager : MonoBehaviour {
+
+    private void Start()
+    {
+        var eventSystem = FindObjectOfType<EventSystem>();
+
+        if (eventSystem == null)
+            Debug.LogError("No UI EventSystem is present in the current scene!");
+    }
+}
