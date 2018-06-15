661a9b23ab7b8c35e8f54bf5115adfea02aa1a28
FireFlame74 <sandro.figo@gmx.at>
2018-06-15 11:48:21 +0200

    Added UI mockup

--- Assets/CLI/Scripts/CLIManager.cs
+++ Assets/CLI/Scripts/CLIManager.cs
@@ -2,8 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CLIManager : MonoBehaviour {
+
+    [SerializeField]
+    private RectTransform m_CLIPanel;
+
+    [SerializeField]
+    private InputField m_InputField;
+
+    [SerializeField]
+    private Text m_SuggestionsText;
+
+    [SerializeField]
+    private Text m_OutputText;
 
     private void Start()
     {
@@ -11,5 +24,20 @@ public class CLIManager : MonoBehaviour {
 
         if (eventSystem == null)
             Debug.LogError("No UI EventSystem is present in the current scene!");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            Debug.Log("Toggled CLI");
+            m_CLIPanel.gameObject.SetActive(!m_CLIPanel.gameObject.activeInHierarchy);
+
+            if (m_CLIPanel.gameObject.activeInHierarchy)
+            {
+                m_InputField.Select();
+                m_InputField.ActivateInputField();
+            }
+        }
     }
 }
