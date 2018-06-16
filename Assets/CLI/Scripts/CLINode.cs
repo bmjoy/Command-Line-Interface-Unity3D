339c83fb8dd6598eb7692832f9290d0a1d75bc80
FireFlame74 <sandro.figo@gmx.at>
2018-06-16 12:11:21 +0200

    Added node class

---
+++ Assets/CLI/Scripts/CLINode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+public class CLINode
+{
+
+    public CLINode() {}
+
+    public CLINode(MethodInfo method, List<CLINode> children, string name)
+    {
+        this.method = method;
+        this.children = children;
+        this.name = name;
+    }
+
+    public MethodInfo method;
+
+    public List<CLINode> children;
+
+    public string name;
+
+}
